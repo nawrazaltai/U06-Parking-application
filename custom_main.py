fe3b4e7261614a2fb0e4a20d5924a3fb94d7f65d
nawrazaltai <nawraz.altai@chasacademy.se>
2022-07-18 21:46:23 +0200

    custom tkinter: created toplevel window (start)

--- custom_main.py
+++ custom_main.py
@@ -13,7 +13,7 @@ from turtle import bgcolor
 from PIL import ImageTk, Image
 from psycopg import Column
 
-customtkinter.set_appearance_mode("light")  # Modes: system (default), light, dark
+customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
 # Create main window called root
@@ -22,8 +22,8 @@ root = customtkinter.CTk()
 root.title("Parking application")
 # root size
 #root.geometry("620x650")
-root.attributes("-fullscreen", True)
-#root.state('zoomed')
+#root.attributes("-fullscreen", True)
+root.state('zoomed')
 # make the window not resizable
 root.resizable(width=False, height=False)
 # background color for window
@@ -57,6 +57,8 @@ lab.grid_columnconfigure(0, weight=1)
 frame_info = customtkinter.CTkFrame(master=root,
                                width=360,
                                height=160,
+                               border_color='white',
+                               border_width=1,
                                corner_radius=8,
                                fg_color='grey')
 frame_info.grid(row=0, column=0, sticky='ne', pady=10, padx=20)
@@ -73,6 +75,18 @@ grey_frame = customtkinter.CTkFrame(master=root,
                                fg_color='grey')
 grey_frame.grid(row=1, column=0, padx=0, pady=0, sticky='w')
 
+# Function to disable root buttons
+def disable_main_buttons():
+    start_button.configure(state=tkinter.DISABLED)
+    stop_button.configure(state=tkinter.DISABLED)
+    status_button.configure(state=tkinter.DISABLED)
+
+# Function to activate root buttons
+def enable_main_buttons():
+    start_button.configure(state=tkinter.NORMAL)
+    stop_button.configure(state=tkinter.NORMAL)
+    status_button.configure(state=tkinter.NORMAL)
+
 # # Function to show date and keep time dynamic at root
 def current_time_date():
     """This function is for the main menu (root).
@@ -80,11 +94,10 @@ def current_time_date():
     It keeps the time dynamic and stores it in the time_label.
 
     The function also shows the current date and stores it in the date_label."""
+    # Store current timestamp in the my_time variable
     my_time = strftime(' Local time: %H:%M:%S')
-    # time_label.config(text=str(my_time), font=("Verdana", 10), fg="White", bg="black")
-    # time_label.after(1000, current_time_date)
-    # todays_date = strftime('Date: %d/%m/%y')
-    # date_label.config(text=str(todays_date), font=("Verdana", 10), fg="White", bg="black")
+
+    # Create label for time and keep it dynamic
     time_label = customtkinter.CTkLabel(master=frame_info,
                                text=f'{my_time}',
                                text_color= "black",
@@ -97,7 +110,10 @@ def current_time_date():
     time_label.after(1000, current_time_date)
     time_label.grid(row=0, column=1, sticky='nw', padx= 20, pady=5)
 
-    todays_date = strftime('Date: %d-%m-%Y')
+    # Store current date in the todays_date variable
+    todays_date = strftime('Date: %A %d-%m-%Y')
+
+    # Create label for date
     date_label = customtkinter.CTkLabel(master=frame_info,
                                text=f' {todays_date}',
                                text_color= "black",
@@ -112,6 +128,7 @@ def current_time_date():
 # Call the time_date function to show time & date on main screen
 current_time_date()
 
+# Create label for availabe parking spots
 space_label = customtkinter.CTkLabel(master=frame_info,
                                text=f"Available parking spots: {TOTAL_PARKING_SPACES}",
                                text_color= "indigo",
@@ -123,75 +140,131 @@ space_label = customtkinter.CTkLabel(master=frame_info,
                                corner_radius=5)
 space_label.grid(row=2, column=1, sticky='sw', padx= 20, pady=5)
 
-welcome_label = customtkinter.CTkLabel(master=root,
+# Create frame for root buttons
+buttons_frame = customtkinter.CTkFrame(master=root,
+                               width=780,
+                               height=350,
+                               border_width=2,
+                               border_color='white',
+                               corner_radius=15,
+                               bg_color='silver',
+                               fg_color='#757575')
+buttons_frame.grid(row=3, column=0, padx=0, pady=30, sticky='')
+
+# Create label for welcome message
+welcome_label = customtkinter.CTkLabel(master=buttons_frame,
                                text="Welcome to YourPark!",
                                text_color= "black",
                                text_font=("Verdana 15 bold"),
                                width=255,
                                height=40,
-                               fg_color=("silver"),
-                               bg_color=("silver"),
+                               fg_color=("#757575"),
+                               bg_color=("#757575"),
                                corner_radius=1)
-welcome_label.grid(row=3, column=0, sticky='s', padx=105, pady=8)
+welcome_label.grid(row=3, column=1, sticky='n', padx=105, pady=8)
 
+# Resize start logo
 start_logo = ImageTk.PhotoImage(Image.open("parking-icon-9.jpg").resize((90, 90), Image.ANTIALIAS))
 
-def start_button():
-    print("button pressed")
-button = customtkinter.CTkButton(master=root,
-                                 width=380,
+# Start parking function
+def start_parking_button():
+
+    # Create new window called start_pop_up for start parking-button
+    start_pop_up = customtkinter.CTkToplevel(root)
+    start_pop_up.iconbitmap('phouse.ico')
+    start_pop_up.title("Start parking")
+    start_pop_up.geometry("400x200")
+    start_pop_up.resizable(width=False, height=False)
+    start_pop_up.config(bg="silver")
+
+    # Disable root buttons
+    disable_main_buttons()
+
+
+
+start_button = customtkinter.CTkButton(master=buttons_frame,
+                                 width=360,
                                  height=80,
                                  border_width=2,
                                  corner_radius=10,
                                  fg_color='#378936',
                                  hover_color='#30694B',
-                                 border_color='black',
+                                 border_color='white',
                                  text="Start parking",
                                  text_color='white',
+                                 text_color_disabled='grey',
                                  text_font='Verdana 12 bold',
                                  image=start_logo,
                                  compound='right',
-                                 command=start_button)
-button.grid(column=0, row=4, pady=20, padx=50, sticky='w')
+                                 command=start_parking_button)
+start_button.grid(column=0, row=4, pady=20, padx=5, sticky='w')
 
-status_logo = ImageTk.PhotoImage(Image.open("status.png").resize((100, 100), Image.ANTIALIAS))
-def status_button():
+# Resize status logo
+status_logo = ImageTk.PhotoImage(Image.open("checkout.png").resize((100, 100), Image.ANTIALIAS))
+
+def status_parking_button():
     print("button pressed")
-button = customtkinter.CTkButton(master=root,
+status_button = customtkinter.CTkButton(master=buttons_frame,
                                  width=380,
                                  height=80,
                                  border_width=2,
                                  corner_radius=10,
-                                 fg_color='#1035AC',
-                                 hover_color='#0E4D92',
-                                 border_color='black',
-                                 text="Car status",
+                                 fg_color='#2565AE', #009dff
+                                 hover_color='#253DA1',
+                                 border_color='white',
+                                 text="Parked car status",
                                  text_color='white',
+                                 text_color_disabled='grey',
                                  text_font='Verdana 12 bold',
                                  image=status_logo,
                                  compound='right',
-                                 command=status_button)
-button.grid(column=0, row=4, pady= 0, padx=50)
+                                 command=status_parking_button)
+status_button.grid(column=1, row=4, pady= 0, padx=0)
 
-stop_logo = ImageTk.PhotoImage(Image.open("checkout.png").resize((100, 100), Image.ANTIALIAS))
-def stop_button():
+# Resize stop logo
+stop_logo = ImageTk.PhotoImage(Image.open("barrier.png").resize((80, 80), Image.ANTIALIAS))
+
+def stop_parking_button():
     print("button pressed")
-button = customtkinter.CTkButton(master=root,
-                                 width=380,
+stop_button = customtkinter.CTkButton(master=buttons_frame,
+                                 width=360,
                                  height=80,
                                  border_width=2,
                                  corner_radius=10,
-                                 fg_color='red',
+                                 fg_color='#D1001F',
                                  hover_color='#BC5449',
-                                 border_color='black',
+                                 border_color='white',
                                  text="Checkout",
                                  text_color='white',
+                                 text_color_disabled='grey',
                                  text_font='Verdana 12 bold',
                                  image=stop_logo,
                                  compound='right',
-                                 command=stop_button)
-button.grid(column=0, row=4, pady= 0, padx=50, sticky='e')
+                                 command=stop_parking_button)
+stop_button.grid(column=2, row=4, pady= 0, padx=5, sticky='e')
+
+# Create frame inside buttons_frame for the price list information
+price_frame = customtkinter.CTkFrame(master=buttons_frame,
+                               width=500,
+                               height=150,
+                               corner_radius=10,
+                               border_width=3,
+                               border_color='white',
+                               fg_color='grey')
+price_frame.grid(row=5, column=1, padx=0, pady=30, sticky='n')
+
+# Create label for the price list
+price_label = customtkinter.CTkLabel(master=buttons_frame,
+                               text="Price list:\n\nMinute 0 - 60 are FREE of charge.\n"
+                               "\n\nFrom minute 61 onwards the price is 0.25 SEK/min (15 SEK/hour).",
+                               text_color= "black",
+                               text_font=("Verdana 10"),
+                               width=10,
+                               height=40,
+                               bg_color=("grey"),
+                               corner_radius=10)
+price_label.grid(row=5, column=1, sticky='n', padx=0, pady=50)
 
 
-
+# run the application
 root.mainloop()
